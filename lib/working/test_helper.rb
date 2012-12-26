2aeac809c20b9fbae5c1901c3e6bf9e691425ef2
☈king <rking@sharpsaw.org>
2012-12-26 09:33:14 -0600

    Move 'turn' to correct spot

--- lib/working/test_helper.rb
+++ lib/working/test_helper.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
+require 'turn'
 if ENV['test_rescue']
   require 'pry-rescue/minitest'
 else
   warn "export test_rescue=1 # to get pry-rescue on test failures"
 end
-require 'turn'
 $:.unshift './lib'
 Turn.config do |c|
   c.format  = :pretty
